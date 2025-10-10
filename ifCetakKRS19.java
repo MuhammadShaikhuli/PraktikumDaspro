698c55f644cb09b0b106725017cbe00a9d855469
MuhammadShaikhuli <muhammadshaikhuli@gmail.com>
2025-10-10 08:17:32 +0700

    Percobaan 1

---
+++ ifCetakKRS19.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ifCetakKRS19 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- CETAK KRS SIAKAD---");
+        System.out.print("Apakah UKT sudah lunas? (TRUE/FALSE): ");
+        boolean ukt = sc.nextBoolean();
+
+        if (ukt) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.print("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+    }}
